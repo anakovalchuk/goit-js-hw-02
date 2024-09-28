b192c0361b49f46b327b9a983f18792433fbb4d1
anakovalchuk <annakovalchuk2008@gmail.com>
2024-09-28 22:13:50 +0200

    Initial

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,22 @@
-function getElementWidth (content, padding, border) {
-const numContent = Number.parseFloat(content);
-const numPadding = Number.parseFloat(padding);
-const numBorder = Number.parseFloat(border);
-const res = numContent + (numPadding + numBorder) * 2;
-return res;
+function checkForSpam(message) {
+    const word1 = "spam";
+    const word2 = "sale";
+   if (message.toLowerCase().includes(word1)){
+    return true
+   } else if (message.toLowerCase().includes(word2)) {
+    return true;
+   } else {
+    return false;
+   }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
